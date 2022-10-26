8a5e2a58f802d245279683250901d57e5e1ba412
0xngmi <0xngmi@protonmail.com>
2022-10-26 19:32:18 +0100

    fix body parsing

--- src/registerEmail.ts
+++ src/registerEmail.ts
@@ -4,7 +4,7 @@ import { execute } from "./utils/sql";
 const handler = async (
     event: AWSLambda.APIGatewayEvent
 ): Promise<any> => {
-    const { email, address } = JSON.parse(event.body!);
+    const { email, address } = JSON.parse(Buffer.from(event.body!, 'base64').toString("utf8"));
     const secret = randomBytes(40).toString('hex');
     await execute('INSERT INTO `emails` VALUES (?, ?, ?, ?)', [Date.now(), email, address.toLowerCase(), secret])
     return {
