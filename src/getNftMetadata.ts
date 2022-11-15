2b7a83b957a93401f273ac793aaca8423ffa6e3b
0xngmi <0xngmi@protonmail.com>
2022-11-15 21:55:09 +0000

    move nft image url

--- src/getNftMetadata.ts
+++ src/getNftMetadata.ts
@@ -50,7 +50,7 @@ const handler = async (
         body: JSON.stringify({
             "name": `LlamaLend Collateral: ${metadata.name}`,
             "description": "NFT used for collateral in LlamaLend. Be careful when buying cause the loan for this NFT might have already expired!",
-            "image": `https://nft.llamalend.com/image/${deadline}/${Buffer.from(metadata.image).toString('base64')}`,
+            "image": `https://nft.llamalend.com/image2/${deadline}/${Buffer.from(metadata.image).toString('base64')}`,
             "attributes": metadata.attributes
         }),
         headers: {
